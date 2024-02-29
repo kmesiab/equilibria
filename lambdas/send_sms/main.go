bc4a8fdfaf63dbb3d0577e92068624bd85f4cefe
Kevin Mesiab <kmesiab@gmail.com>
2024-02-29 11:21:53 -0800

    Add sender lambda

---
+++ lambdas/send_sms/main.go
@@ -0,0 +1,258 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"slices"
+	"strconv"
+	"time"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+
+	"github.com/kmesiab/equilibria/lambdas/lib"
+	"github.com/kmesiab/equilibria/lambdas/lib/ai"
+	"github.com/kmesiab/equilibria/lambdas/lib/config"
+	"github.com/kmesiab/equilibria/lambdas/lib/db"
+	"github.com/kmesiab/equilibria/lambdas/lib/log"
+	"github.com/kmesiab/equilibria/lambdas/lib/message"
+	"github.com/kmesiab/equilibria/lambdas/lib/twilio"
+	"github.com/kmesiab/equilibria/lambdas/lib/utils"
+	"github.com/kmesiab/equilibria/lambdas/models"
+)
+
+// How many past memories to include in the prompt
+const maxMemories = 140
+
+// How many immediately previous messages to include in the prompt
+const maxLastFewMessages = 10
+
+type SendSMSLambdaHandler struct {
+	lib.LambdaHandler
+	MemoryService     *message.MemoryService
+	CompletionService ai.CompletionServiceInterface
+}
+
+func (h *SendSMSLambdaHandler) HandleRequest(sqsEvent events.SQSEvent) {
+
+	var (
+		err error
+		now = time.Now()
+		msg models.Message
+
+		formattedDate = now.Format("January 2, 2006 3:04pm")
+	)
+
+	if err = ValidateEvent(&sqsEvent); err != nil {
+		log.New("Error validating the event").AddError(err).Log()
+
+		return
+	}
+
+	var event = sqsEvent.Records[0]
+	var body = event.Body
+
+	if err = json.Unmarshal([]byte(body), &msg); err != nil {
+		log.New("Error unmarshalling message").AddError(err).Log()
+
+		return
+	}
+
+	// Get the sender's user account
+	recipient, err := h.UserService.GetUserByID(msg.FromUserID)
+
+	if err != nil {
+		log.New("Could not locate user %d.  Rejecting.", msg.FromUserID).
+			AddSQSEvent(&event).AddError(err).AddMessage(&msg).Log()
+
+		return
+	}
+
+	// Make sure the sender is not the system user
+	if recipient.ID == models.GetSystemUser().ID {
+		log.New("Message is from system user. Aborting.").
+			AddUser(recipient).AddSQSEvent(&event).AddError(err).AddMessage(&msg).Log()
+
+		return
+	}
+
+	log.New("Preparing a response for %s", recipient.PhoneNumber).
+		AddUser(recipient).AddSQSEvent(&event).AddMessage(&msg).Log()
+
+	// Get the memories for the user
+	memories, err := h.GetMemories(recipient, event, msg)
+
+	if err != nil {
+		log.New("Error remembering history %s", err.Error()).
+			AddUser(recipient).AddSQSEvent(&event).AddError(err).AddMessage(&msg).Log()
+
+		return
+	}
+
+	prompt := fmt.Sprintf(ConditioningPrompt, formattedDate, recipient.Firstname)
+
+	log.New("Generated Prompt").Add("prompt", prompt).
+		AddUser(recipient).AddSQSEvent(&event).AddMessage(&msg).
+		Add("memory_count", strconv.Itoa(len(memories))).Log()
+
+	// Send the prompt for completion
+	completion, err := h.CompletionService.GetCompletion(msg.Body, prompt, &memories)
+
+	if err != nil {
+		log.New("Error getting completion").Add("prompt", prompt).
+			AddUser(recipient).AddSQSEvent(&event).AddError(err).AddMessage(&msg).
+			Add("memory_count", strconv.Itoa(len(memories))).Log()
+
+		return
+	}
+
+	// Create a message entry in the db
+	newMessage := NewMessage(&msg)
+	newMessage.ConversationID = msg.ConversationID
+	newMessage.FromUserID = models.GetSystemUser().ID
+	newMessage.ToUserID = recipient.ID
+	newMessage.SentAt = &now
+	newMessage.MessageStatus = models.NewMessageStatusSending()
+
+	newMessage.Body = completion
+	err = h.MessageService.CreateMessage(newMessage)
+
+	if err != nil {
+		log.New("Error saving new message").
+			AddUser(recipient).AddSQSEvent(&event).AddError(err).AddMessage(&msg).Log()
+
+		return
+	}
+
+	log.New("Sending SMS from %s to %s",
+		newMessage.From.PhoneNumber,
+		recipient.PhoneNumber).
+		Add("message", newMessage.Body).
+		Log()
+
+	// Send the message back to the sender
+	smsResponse, err := twilio.SendSMS(
+		models.GetSystemUser().PhoneNumber, recipient.PhoneNumber, completion,
+	)
+
+	if err != nil {
+		log.New("Error: Sending sms message").
+			AddUser(recipient).AddSQSEvent(&event).AddError(err).AddMessage(&msg).Log()
+
+		return
+	}
+
+	if smsResponse == nil {
+		log.New("Error: SMS Response is empty").
+			AddUser(recipient).AddSQSEvent(&event).AddError(err).AddMessage(&msg).Log()
+
+		return
+	}
+
+	// Update the message with its SID from twilio
+	newMessage.ReferenceID = smsResponse.Sid
+	err = h.MessageService.UpdateMessage(newMessage)
+
+	if err != nil {
+		log.New("Error: Updating new message with reference ID").
+			AddUser(recipient).AddSQSEvent(&event).AddError(err).AddMessage(&msg).Log()
+
+		return
+	}
+
+	log.New("Successfully queued outbound message from %s to %s",
+		msg.MessageType.Name, *smsResponse.To).
+		AddSmsResponse(smsResponse).
+		Log()
+}
+
+func (h *SendSMSLambdaHandler) GetMemories(recipient *models.User, event events.SQSMessage, msg models.Message) ([]models.Message, error) {
+
+	lastFewMemories, err := h.MemoryService.GetLastNMessagePairs(recipient, maxLastFewMessages)
+
+	if err != nil {
+		log.New("Error retrieving last few memories for user %s", recipient.PhoneNumber).
+			AddUser(recipient).AddSQSEvent(&event).AddError(err).AddMessage(&msg).Log()
+
+		return nil, err
+	}
+
+	aFewOlderMemories, err := h.MemoryService.GetRandomMessagePairs(recipient, maxMemories)
+
+	if err != nil {
+		log.New(
+			"Error retrieving a few older memories for user %s",
+			recipient.PhoneNumber).Log()
+
+		return nil, err
+	}
+
+	memories := append(*lastFewMemories, *aFewOlderMemories...)
+	slices.Reverse(memories)
+
+	return memories, nil
+}
+
+func NewMessage(incomingMessage *models.Message) *models.Message {
+
+	return &models.Message{
+		FromUserID:      models.GetSystemUser().ID,
+		ToUserID:        incomingMessage.FromUserID,
+		ReferenceID:     incomingMessage.ReferenceID,
+		MessageType:     models.NewMessageTypeSMS(),
+		MessageStatusID: models.NewMessageStatusSent().ID,
+	}
+}
+
+func ValidateEvent(sqsEvent *events.SQSEvent) error {
+	if len(sqsEvent.Records) == 0 {
+
+		return fmt.Errorf("no event records found")
+	}
+
+	if sqsEvent.Records[0].Body == "" {
+
+		return fmt.Errorf("no event body found")
+	}
+
+	return nil
+}
+
+func main() {
+
+	log.New("SMS Sender Lambda booting.....").Log()
+
+	cfg := config.Get()
+
+	if cfg == nil {
+		log.New("Could not load config")
+	}
+
+	database := db.Get(cfg)
+
+	if err := utils.PingDatabase(database); err != nil {
+		log.New("Error pinging database").AddError(err).Log()
+
+		return
+	}
+
+	if err := utils.PingGoogle(); err != nil {
+		log.New("Error pinging Google. Possible bad internet connection.").AddError(err).Log()
+
+		return
+	}
+
+	memoryService := message.NewMemoryService(
+		message.NewMessageRepository(database), maxMemories,
+	)
+
+	handler := &SendSMSLambdaHandler{
+		CompletionService: &ai.OpenAICompletionService{},
+		MemoryService:     memoryService,
+	}
+
+	handler.Init(database)
+
+	log.New("SMS Sender Lambda ready. Invoking.").Log()
+	lambda.Start(handler.HandleRequest)
+}
---
+++ lambdas/send_sms/main_test.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/stretchr/testify/require"
+
+	"github.com/kmesiab/equilibria/lambdas/lib/ai"
+	"github.com/kmesiab/equilibria/lambdas/lib/message"
+	"github.com/kmesiab/equilibria/lambdas/lib/test"
+)
+
+func TestHandleRequest_NoBody(t *testing.T) {
+	test.SetEnvVars()
+	event := events.SQSEvent{
+		Records: []events.SQSMessage{{
+			Body: "",
+		}},
+	}
+
+	db, _, err := test.SetupMockDB()
+
+	require.NoError(t, err, "Could not run tests, could nto set up mock db")
+
+	handler := &SendSMSLambdaHandler{
+		CompletionService: &ai.OpenAICompletionService{},
+		MemoryService: &message.MemoryService{
+			MessageService: message.NewMessageService(message.NewMessageRepository(db)),
+		},
+	}
+
+	handler.Init(db)
+	handler.HandleRequest(event)
+}
+
+func TestHandleRequest(t *testing.T) {
+	test.SetEnvVars()
+	event := events.SQSEvent{
+		Records: []events.SQSMessage{{
+			Body: "{\r\n\t\"id\": 1,\r\n\t\"reference_id\": \"SAxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx\",\r\n\t\"conversation_id\": 5,\r\n\t\"from_user_id\": 3,\r\n\t\"to_user_id\": 1,\r\n\t\"body\": \"Hey, I was wondering if you thought maybe i'd be getting that promotion?\",\r\n\t\"message_type_id\": 2,\r\n\t\"message_status_id\": 3,\r\n\t\"sent_at\": null,\r\n\t\"received_at\": \"2023-12-14T09:32:15Z\",\r\n\t\"created_at\": \"2023-12-14T09:32:15Z\",\r\n\t\"updated_at\": \"2023-12-14T09:32:15Z\",\r\n\t\"deleted_at\": null,\r\n\t\"conversation\": {\r\n\t\t\"user\": {\r\n\t\t\t\"status\": {\r\n\t\t\t\t\"status_id\": 2,\r\n\t\t\t\t\"status_name\": \"Active\"\r\n\t\t\t},\r\n\t\t\t\"id\": 3,\r\n\t\t\t\"phone_number\": \"+12533243071\",\r\n\t\t\t\"phone_verified\": true,\r\n\t\t\t\"firstname\": \"New Name\",\r\n\t\t\t\"lastname\": \"Mesiab\",\r\n\t\t\t\"email\": \"kmesiab+equilibria_sms@gmail.com\",\r\n\t\t\t\"account_status_id\": 2\r\n\t\t},\r\n\t\t\"id\": 5,\r\n\t\t\"user_id\": 3,\r\n\t\t\"start_time\": \"2023-12-14T09:32:15Z\",\r\n\t\t\"end_time\": null,\r\n\t\t\"created_at\": \"2023-12-14T09:32:15Z\",\r\n\t\t\"updated_at\": \"0001-01-01T00:00:00Z\"\r\n\t},\r\n\t\"message_status\": {\r\n\t\t\"status_id\": 3,\r\n\t\t\"status_name\": \"Received\"\r\n\t},\r\n\t\"message_type\": {\r\n\t\t\"id\": 2,\r\n\t\t\"name\": \"SMS\",\r\n\t\t\"bill_rate_in_credits\": 1\r\n\t},\r\n\t\"from\": {\r\n\t\t\"status\": {\r\n\t\t\t\"status_id\": 2,\r\n\t\t\t\"status_name\": \"Active\"\r\n\t\t},\r\n\t\t\"id\": 5,\r\n\t\t\"phone_number\": \"+12533243071\",\r\n\t\t\"phone_verified\": true,\r\n\t\t\"firstname\": \"New Name\",\r\n\t\t\"lastname\": \"Mesiab\",\r\n\t\t\"email\": \"kmesiab+equilibria_sms@gmail.com\",\r\n\t\t\"account_status_id\": 2\r\n\t},\r\n\t\"to\": {\r\n\t\t\"status\": {\r\n\t\t\t\"status_id\": 2,\r\n\t\t\t\"status_name\": \"Active\"\r\n\t\t},\r\n\t\t\"id\": 1,\r\n\t\t\"phone_number\": \"+18333595081\",\r\n\t\t\"phone_verified\": true,\r\n\t\t\"firstname\": \"System\",\r\n\t\t\"lastname\": \"User\",\r\n\t\t\"email\": \"-@-\",\r\n\t\t\"account_status_id\": 2\r\n\t}\r\n}",
+		}},
+	}
+
+	db, mock, err := test.SetupMockDB()
+
+	mock.ExpectQuery("SELECT \\* FROM `users` WHERE `users`.`id`").
+		WithArgs(3).
+		WillReturnRows(sqlmock.NewRows([]string{"id", "email"}).
+			AddRow(1, test.DefaultTestEmail))
+
+	require.NoError(t, err, "Could not run tests, could nto set up mock db")
+
+	handler := &SendSMSLambdaHandler{
+		CompletionService: &ai.OpenAICompletionService{},
+		MemoryService: &message.MemoryService{
+			MessageService: message.NewMessageService(message.NewMessageRepository(db)),
+		},
+	}
+
+	handler.Init(db)
+	handler.HandleRequest(event)
+}
