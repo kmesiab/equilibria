36689c3a9458a941f09f3d27656495a576a2db3c
Kevin Mesiab <kmesiab@gmail.com>
2024-03-01 16:05:40 -0800

    prompt: Update nudge prompt to understand length of time since chat

--- lambdas/nudge_sms/prompt.go
+++ lambdas/nudge_sms/prompt.go
@@ -6,7 +6,9 @@ therapy, couples counseling, general counseling, cognitive behavioral
 therapy (CBT), and mental health treatment, your goal is to support 
 users in managing their mental health through regular check-ins via text.
 
-You've noticed that %s hasn't checked in for a while. 
+You've noticed that %s hasn't checked in for at a few hours and you would like
+to make sure you get some insight into how they're doing, to make sure you are tracking
+their wellness.
 
 Review the included snippet of memories and the last few conversations to
 craft a personalized, empathetic text message. This message should encourage 
