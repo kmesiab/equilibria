7cc69e979a88b09b4c52887b7f42d925beee50d9
Kevin Mesiab <kmesiab@gmail.com>
2024-05-26 22:37:34 -0700

    enhancement: Encourage fact finder to be more contextually relevant

--- lambdas/lib/ai/agents/fact_agent/backstory.go
+++ lambdas/lib/ai/agents/fact_agent/backstory.go
@@ -8,7 +8,7 @@ be relevant for a therapist managing a patient's mental health. Not all messages
 will include facts worth remembering.
 
 If there are important facts, condense each into a summary while maintaining 
-as much relevant context as possible.  If a message is instructive, include it.  
+as much relevant context as possible. If a message is instructive, include it. 
 If there are no important facts, do not include it.
 
 Additionally, provide a section explaining why each fact is relevant to the 
@@ -22,18 +22,16 @@ func getBackStory() string {
 }
 
 func getExampleJSONResponseText() string {
-
 	return fmt.Sprintf(
 		jsonTextBlockTemplate,
 		exampleResponseText,
 		exampleNoResponseJson,
 		fullExampleResponseText,
 	)
-
 }
 
 const jsonTextBlockTemplate = "```json\n%s\n```" +
-	`If there are no facts worth remembering, respond with with:` +
+	`If there are no facts worth remembering, respond with:` +
 	"\n```json\n%s\n```" + // exampleNoResponseJson
 
 	`**Example: **
@@ -47,15 +45,15 @@ const jsonTextBlockTemplate = "```json\n%s\n```" +
 const exampleNoResponseJson = `[]`
 const exampleResponseText = `
 [
-        {
-            "fact": "Summarized fact",
-            "reasoning": "Explanation of why this is relevant to the therapist"
-        },
-        {
-            "fact": "Summarized fact",
-            "reasoning": "Explanation of why this is relevant to the therapist"
-        }
-    ]
+	{
+		"fact": "Summarized fact",
+		"reasoning": "Explanation of why this is relevant to the therapist"
+	},
+	{
+		"fact": "Summarized fact",
+		"reasoning": "Explanation of why this is relevant to the therapist"
+	}
+]
 `
 const fullExampleResponseText = `
 [
